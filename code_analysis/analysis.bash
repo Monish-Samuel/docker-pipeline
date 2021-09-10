e4b780ccf016a9d7f4586cf8faf724bd1433cb06
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-09-10 15:52:22 +0530

    Update analysis.bash

--- code_analysis/analysis.bash
+++ code_analysis/analysis.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "--------------------Radon Code Analysis Start----------------------------"
-mic= $(radon cc ../src/Alphabet.py)
-echo $mic
+radon cc ../src/Alphabet.py
 radon cc ../src/output.py
 radon cc ../src/test_Alphabet.py
 echo "--------------------Radon Code Analysis Stop----------------------------"
