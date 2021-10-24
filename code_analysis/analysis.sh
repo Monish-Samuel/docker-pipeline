8b4b281705c4ae02b5ef8dd0b6c2518977365449
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-10-24 16:00:25 +0530

    Update analysis.sh

--- code_analysis/analysis.sh
+++ code_analysis/analysis.sh
@@ -3,13 +3,13 @@ echo "--------------------Radon Code Analysis Start----------------------------"
 declare -a strings=("Alphabet.py" "output.py" "test_Alphabet.py")
 for i in "${strings[@]}";do
 	radon cc src/$i
-	echo "Sleep 5s" && sleep 5
+	echo "Sleep 3s" && sleep 3
 done
 echo "--------------------Radon Code Analysis Stop----------------------------"
-echo "Sleep 10s" && sleep 10
+echo "Sleep 5s" && sleep 5
 echo "--------------------Pylint Code Analysis Start----------------------------"
 for i in "${strings[@]}";do
 	pylint src/$i --reports=y
-	echo "Sleep 5s" && sleep 5
+	echo "Sleep 3s" && sleep 3
 done
 echo "--------------------Pylint Code Analysis Stop----------------------------"
